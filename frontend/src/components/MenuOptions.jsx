55f9361cce8177e88b6a75bd85707bbc7a2ff7d9
enyeldev <medranogarciaenyel@gmail.com>
2024-03-14 18:04:44 -0400

    menu-list-arreglado

--- frontend/src/components/MenuOptions.jsx
+++ frontend/src/components/MenuOptions.jsx
@@ -1,23 +1,39 @@
 /* eslint-disable react/prop-types */
 
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { IoChevronDown } from "react-icons/io5";
 
-
 export const MenuOptions = ({ icon, name, arrOptions }) => {
-  const menuRef = useRef(null);
+  const ref = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setIsVisible(false);
+      }
+    };
+
+    // Agregar un event listener para detectar clics fuera del elemento
+    document.addEventListener("click", handleClickOutside);
+
+    // Eliminar el event listener al desmontar el componente
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   //   Callback que muestra la lista de opciones
-  const mostrarListaOpciones = () => {
-    setIsVisible(!isVisible);
-  };
+  // const mostrarListaOpciones = () => {
+  //   setIsVisible(!isVisible);
+  // };
 
   return (
-    <div className="relative ">
+    <div className="relative menu-desplegable">
       <div
-        className="text-lg text-white flex items-center justify-between p-2 rounded-md hover:bg-blue-800 transition-colors duration-150 cursor-pointer"
-        ref={menuRef}
-        onClick={mostrarListaOpciones}
+        ref={ref}
+        className="text-lg text-white flex items-center justify-between p-2 rounded-md hover:bg-blue-800 transition-colors duration-150 cursor-pointer "
+        onClick={() => setIsVisible(!isVisible)}
       >
         <div className="flex gap-3">
           {icon}
@@ -27,7 +43,7 @@ export const MenuOptions = ({ icon, name, arrOptions }) => {
       </div>
       {isVisible && (
         <div
-          className={`w-full absolute top-12 left-0 z-10 p-2 bg-blue-600 rounded-md shadow-md animate-fade-down animate-once animate-ease-in-out ${
+          className={`w-full absolute top-12 left-0 z-10 p-2 bg-blue-600 rounded-md shadow-md animate-fade-down animate-once animate-ease-in-out${
             isVisible ? "" : ""
           }`}
         >
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-
+import tailwindcssAnimate from "tailwindcss-animate";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
